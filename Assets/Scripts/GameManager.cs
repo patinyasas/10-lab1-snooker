7bfc01b31c424a528a330b77677e3c01d35f78bf
patinyasas <patinyasas.auns@bumail.net>
2023-11-23 16:36:34 +0700

    stop ball

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,12 +15,15 @@ public class GameManager : MonoBehaviour
 
         [SerializeField] private float xInput;
         [SerializeField] private float force;
+        [SerializeField] private new GameObject camera;
 
 
         // Start is called before the first frame update
         void Start()
         {
             instance = this;
+            camera = camera.gameObject;
+            CameraBehindBall();
             
             SetBalls(BallColors.White, 0);
             SetBalls(BallColors.Red, 1);
@@ -39,6 +42,11 @@ public class GameManager : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     ShootBall();
+                }
+
+                if (Input.GetKeyDown(KeyCode.Backspace))
+                {
+                    StopBall();
                 }
                 
         }
@@ -59,8 +67,26 @@ public class GameManager : MonoBehaviour
         // ReSharper disable Unity.PerformanceAnalysis
         void ShootBall()
         {
+            camera.transform.parent = null;
             Rigidbody rd = cueBall.GetComponent<Rigidbody>();
             rd.AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
             ballLine.SetActive(false);
+        }
+
+        void CameraBehindBall()
+        {
+            camera.transform.parent = cueBall.transform;
+            camera.transform.position = cueBall.transform.position + new Vector3(0f, 20f, -15f);
+        }
+
+        void StopBall()
+        {
+            Rigidbody rd = cueBall.GetComponent<Rigidbody>();
+            rd.velocity = Vector3.zero;
+            rd.angularVelocity = Vector3.zero;
+            cueBall.transform.eulerAngles = Vector3.zero;
+            CameraBehindBall();
+            camera.transform.eulerAngles = new Vector3(40f, 0f, 0f);
+            ballLine.SetActive(true);
         }
     }
